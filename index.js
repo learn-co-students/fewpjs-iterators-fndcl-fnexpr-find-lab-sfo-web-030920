93b321983bd6078e78229c8ff437c56501e9c201
caicindy87 <caicindy87@gmail.com>
2020-04-28 15:34:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
-const testVar = {};
-
-function testFunc() {
-  return "hi";
-}
-
 function superbowlWin(array) {
   const winRecord = array.find((record) => {
     return record.result === "W";
@@ -11,4 +5,10 @@ function superbowlWin(array) {
   if (winRecord) {
     return winRecord.year;
   }
+}
+
+// More concise
+function superbowlWin(array) {
+  const winRecord = array.find((record) => record.result === "W");
+  return winRecord ? winRecord.year : undefined;
 }
