c6ee285672f97d3324ac372f9f9dec9dbc35f185
SaberChulainn <Tbandachit@gmail.com>
2020-04-29 16:20:44 -0700

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(a) {
+  let win = a.find((record) => {
+    if (record.result == "W") {
+      return record
+    }
+  });
+  if(win)
+  return win.year;
+}
